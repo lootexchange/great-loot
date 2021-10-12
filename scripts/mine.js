5d6f0fbf1bef5b05f33ecbf105992440dbb4a043
Peter Watts <pwattsmail@gmail.com>
2021-10-13 09:28:28 +1100

    more efficient

--- scripts/mine.js
+++ scripts/mine.js
@@ -4,23 +4,22 @@ const { id } = require("@ethersproject/hash");
 const random = input => BigNumber.from(id(input));
 const items = ['WEAPON','CHEST','HEAD','WAIST','FOOT','HAND','NECK','RING'];
 
-const getGreatness = id => {
-    let greatness = 0
-    const tokenId = BigNumber.from(id);
-    for (let keyPrefix of items) {
-        const rand = random(keyPrefix + tokenId.toString());
-        greatness += rand.mod(21).toNumber();
-    }
-    return greatness;
-};
-
-const findGreatBags = (start, end, threshold) => {
+const findGreatBags = (start, end, threshold, itemThreshold) => {
     for(let i=start; i<=end; i++) {
-        let greatness = getGreatness(i);
-            if(greatness>=threshold) {
+        let greatness = 0
+        const tokenId = BigNumber.from(i);
+        for (let keyPrefix of items) {
+            const rand = random(keyPrefix + tokenId.toString());
+            let itemGreatness = rand.mod(21).toNumber();
+            if(itemGreatness<itemThreshold) {
+                break
+            }
+            greatness+=itemGreatness
+        }
+        if(greatness>=threshold) {
             console.log(i,greatness)
         }
     }
 }
 
-findGreatBags(100000000,10000000000,140)
+findGreatBags(106900000,10000000000,140, 18)
